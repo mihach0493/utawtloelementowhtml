ebfa70d0f3c552e52147e6a1b402d457f57edd34
Mykhailo Chui <mihach0493@gmail>
2020-10-04 09:49:32 +0200

    utawtloelementowhtml

---
+++ test.js
@@ -0,0 +1,14 @@
+const setBackground = () => {
+    let p1 = document.querySelector('.first');
+    let p2 = document.querySelector('.last');
+    console.log(p2);
+
+    p1.classList.add('bg-red');
+    p2.classList.add('bg-yellow');
+}
+
+
+let btnSetBackground = document.getElementById('set-background');
+
+
+btnSetBackground.addEventListener('click', setBackground);
